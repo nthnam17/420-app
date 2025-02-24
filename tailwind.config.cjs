d61908d665d043060405ea375f6e1456f573db39
NthnamUtd <nthnam.dev@gmail.com>
2025-02-24 17:33:26 +0700

    feat(init): init first time pro build

---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        primary: '#5475D1',
+        'primary-light': '#C4147A',
+        'primary-edit': '#4B49AC',
+        'primary-yellow': '#f78a1c',
+        'primary-green': '#55b756',
+        'primary-gray': '#637381',
+        'primary-black': '#212b36',
+        'primary-white': '#fff',
+        'primary-border': '#919eab33',
+        success: '#06d6a0',
+        'success-opcity': '#cdf7ec',
+        delete: '#ef476f',
+        'delete-opcity': '#fcdae2',
+        edit: '#1ea6d3',
+        'edit-opcity': '#d2edf6',
+        warning: '#f1be46',
+        'warning-opcity': '#fcf2da'
+      }
+    }
+  },
+  plugins: []
+}
